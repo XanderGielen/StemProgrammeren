13505556938372a8306dcaff8b2487fd889521ec
XanderGielen <146929524+XanderGielen@users.noreply.github.com>
2023-10-10 10:24:01 +0200

    pi n stuff

---
+++ C#/Pie/Pie/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+int N;
+int vakInCirkel = 0;
+
+Console.WriteLine("Geef N: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < N; i++) //lus voor de verticale-as
+{
+    for (int j = 0; j < N; j++)
+    {
+        double x = (i + 0.5) / N;
+        double y = (j + 0.5) / N;
+
+        double z = x * x + y * y;
+
+        if (z < 1)
+        {
+            vakInCirkel++;
+        }
+    }
+}
+
+double pi;
+pi = (double)(vakInCirkel * 4) / (N * N);
+Console.WriteLine(pi);
---
+++ C#/Pitweepunten/Pitweepunten/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+double x;
+double y;
+int vakInCirkel = 0;
+int N;
+Console.WriteLine("Geef hoeveel punten je wilt: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+    Random getalGenerator = new Random();
+    x = getalGenerator.NextDouble();
+    y = getalGenerator.NextDouble();
+
+    double z = x * x + y * y;
+
+    if (z < 1)
+    {
+        vakInCirkel++;
+    }
+}
+
+
+double pi;
+pi = (double)(vakInCirkel * 4) / N;
+Console.WriteLine(pi);
