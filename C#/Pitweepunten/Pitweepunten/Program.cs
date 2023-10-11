fe4814c4ccc35a4b9a7f6f1d00e4d196f5cc9af6
XanderGielen <146929524+XanderGielen@users.noreply.github.com>
2023-10-11 10:56:31 +0200

    updated

--- C#/Pitweepunten/Pitweepunten/Program.cs
+++ C#/Pitweepunten/Pitweepunten/Program.cs
@@ -2,26 +2,34 @@
 
 double x;
 double y;
+double pi = 0;
 int vakInCirkel = 0;
 int N;
+int aantalKeer = 100;
 Console.WriteLine("Geef hoeveel punten je wilt: ");
 N = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 0; i < N; i++)
+for (int j = 0; j < aantalKeer; j++)
 {
-    Random getalGenerator = new Random();
-    x = getalGenerator.NextDouble();
-    y = getalGenerator.NextDouble();
-
-    double z = x * x + y * y;
-
-    if (z < 1)
+    vakInCirkel = 0;
+    for (int i = 0; i < N; i++)
     {
-        vakInCirkel++;
+        Random getalGenerator = new Random();
+        x = getalGenerator.NextDouble();
+        y = getalGenerator.NextDouble();
+
+        double z = x * x + y * y;
+
+        if (z < 1)
+        {
+            vakInCirkel++;
+        }
     }
+
+
+    
+    pi = (double)(vakInCirkel * 4) / N;
+    Console.WriteLine(pi);
 }
 
 
-double pi;
-pi = (double)(vakInCirkel * 4) / N;
-Console.WriteLine(pi);
