dca882c45c13d4609ada05f9ba650b99042a026c
XanderGielen <146929524+XanderGielen@users.noreply.github.com>
2024-01-11 09:14:08 +0100

    dodona oefeningen voor toets

---
+++ Python/lijstdodona1.py
@@ -0,0 +1,5 @@
+lijst = []
+for i in range(3):
+    nummer = int(input("geef het nummer"))
+    lijst.append(nummer)
+print(lijst)
---
+++ Python/lijstdodona2.py
@@ -0,0 +1,7 @@
+lijst = []
+nummer = 0
+while nummer != -1:
+    nummer = int(input("geef het nummer"))
+    lijst.append(nummer)
+lijst.pop()
+print(lijst)
