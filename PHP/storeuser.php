85ba2023475e1bd881cc03a8764d924819e25be9
XanderGielen <146929524+XanderGielen@users.noreply.github.com>
2024-01-16 20:35:21 +0100

    proj school, attempt 2

--- PHP/storeuser.php
+++ PHP/storeuser.php
@@ -1,6 +1,7 @@
 <?php
 
-//create users
+//genereer voorbeeld gebruikers
+/*
 for ($i = 0; $i < 5; $i++) {
     $random = random_int(1, 5);
     
@@ -31,15 +32,26 @@ for ($i = 0; $i < 5; $i++) {
     }
     addUser($user, $tokens);
 }
+*/
 
+
+
+//verhoog of verlaag de tokens van een gebruiker en
+//toon alle gebruikers en het verschil
+echo increaseTokens("K2s5GfH7", 5);
 
 $allUsers = readUsers();
 print_r($allUsers);
 
 echo decreaseTokens("K2s5GfH7", 1);
 
+$allUsers = readUsers();
+print_r($allUsers);
 
 
+
+//voeg een gebruiker toe, het vraagt een userID en hoeveelheid tokens
+//het gaat dan een array maken voor elke token en deze in de file zetten
 function addUser($userID, $tokens)
 {
     $user = array($userID, $tokens);
@@ -47,33 +59,28 @@ function addUser($userID, $tokens)
     fputcsv($file, $user);
     fclose($file);
 }
-// Functie om gebruikersinformatie te lezen
+
+//functie om gebruikersinformatie te lezen
+//het gaat lijn per lijn, zet de gebruikers in een array en toont al deze gebruikers
+//en als er geen users.csv bestand bestaat maakt hij die aan.
 function readUsers()
 {
     $row = 1;
     $users = array();
     if (($file = fopen("users.csv", "r")) !== FALSE) {
-        while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
-            $num = count($data);
-            $row++;
-            for ($c=0; $c < $num; $c++) {
-                echo $data[$c] . "<br />\n";
-            }
+        while (($data = fgetcsv($file, 1000, ",")) !== FALSE)
+        {
+            $users[] = $data;
         }
+
         fclose($file);
     }
     return $users;
 }
 
-function writeUsers($users)
-{
-    $file = fopen("users.csv", "w");
-    foreach ($users as $user) {
-        fputcsv($file, $user);
-    }
-    fclose($file);
-}
-
+//verminder het aantal tokens door lijn per lijn te gaan en te zien of
+// de userID ingegeven is gelijk is aan degene die op die lijn staat
+// en vermindert dan het ingegeven aantal tokens.
 function decreaseTokens($userID, $amount)
 {
     $users = readUsers();
@@ -82,5 +89,26 @@ function decreaseTokens($userID, $amount)
             $user[1] -= $amount;
         }
     }
-    echo($users);
+    writeUsers($users);
+}
+//hetzelfde maar toevoegen
+function increaseTokens($userID, $amount)
+{
+    $users = readUsers();
+    foreach ($users as &$user) {
+        if ($user[0] == $userID) {
+            $user[1] += $amount;
+        }
+    }
+    writeUsers($users);
+}
+
+//dit schrijft alle gebruikers voor de decrease en increase token functies.
+function writeUsers($users)
+{
+    $file = fopen("users.csv", "w");
+    foreach ($users as $user) {
+        fputcsv($file, $user);
+    }
+    fclose($file);
 }
